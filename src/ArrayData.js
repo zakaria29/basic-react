c3c41c5280e0b2ee34f8c7d7864ce257f26c899b
zakaria29 <zakaria@smktelkom-mlg.sch.id>
2021-01-05 10:21:43 +0700

    meet 5/1/2021

---
+++ src/ArrayData.js
@@ -0,0 +1,7 @@
+const alert = [
+    { color: "danger", text: "Awas ada wewe gombel"},
+    { color: "info", text: "Awas ada kuyang"},
+    { color: "success", text: "Selamat menempuh hidup baru"},
+    { color: "warning", text: "hati-hati dijalan keramat"},
+];
+export default alert;
---
+++ src/Main.js
@@ -0,0 +1,23 @@
+import React from "react"
+import Alert from "./components/Alert";
+import data from "./ArrayData";
+class Main extends React.Component{
+    render(){
+        
+        return (
+            <div className="container">
+                <Alert warna="danger" teks="Awas ada hantu" />
+                <Alert warna={data[2].color} teks={data[2].text} />
+
+                { data.map(item => (
+                    <Alert warna={item.color} teks={item.text} />
+                ))}
+                
+                { data.forEach((it,index) => (
+                    <Alert warna={it.color} teks={it.text} />
+                ))}
+            </div>
+        )
+    }
+}
+export default Main;
