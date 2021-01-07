a9e7599c6e0b3695540183badb304ef8960bb5e6
zakaria29 <zakaria@smktelkom-mlg.sch.id>
2021-01-07 08:50:00 +0700

    meet 2 (state)

--- src/Main.js
+++ src/Main.js
@@ -1,18 +1,21 @@
 import React from "react"
 import Alert from "./components/Alert";
 import data from "./ArrayData";
+import Counter from "./components/Counter";
 class Main extends React.Component{
     render(){
         
         return (
             <div className="container">
-                <Alert warna="danger" teks="Awas ada hantu" />
+                {/* <Alert warna="danger" teks="Awas ada hantu" />
                 <Alert warna={data[2].color} teks={data[2].text} />
 
                 { data.map(item => (
                     <Alert warna={item.color} teks={item.text} />
                 ))}
-                
+                 */}
+                 <Counter />
+                 <Counter />
                 
             </div>
         )
---
+++ src/components/Counter.js
@@ -0,0 +1,56 @@
+import React from "react"
+class Counter extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            count: 10, 
+        }
+
+        /**
+         * saat ingin akses data state -> this.state.state_name
+         * saat ingin mengubah data state -> this.setState({ state_name: value })
+         * 
+         * this.state.count = 9 (cara yg salah utk mengubah state)
+         */ 
+    }
+
+    increment = () => {
+        let c = this.state.count;
+        // merubah nilai state menggunakan fungsi setState
+        this.setState({
+            count: ++c
+        })
+    }
+
+    decrement = () => {
+        let c = this.state.count;
+        // merubah nilai state menggunakan fungsi setState
+        this.setState({
+            count: --c
+        })
+    }
+
+    render(){
+        return (
+            <div className="row col-5">
+                <div className="col-2">
+                <button className="btn btn-danger btn-block"
+                onClick={this.decrement}>
+                    -
+                </button>
+                </div>
+                <div className="col-8">
+                    <input type="number" className="form-control"
+                    value={this.state.count} readOnly />
+                </div>
+                <div className="col-2">
+                    <button className="btn btn-success btn-block"
+                    onClick={this.increment}>
+                         + 
+                    </button>
+                </div>        
+            </div>
+        )
+    }
+}
+export default Counter;
